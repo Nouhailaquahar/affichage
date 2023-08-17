bc6e1ddeada55bf9c99ee4abef22309cbd2eadd1
Nouhailaquahar <98102558+Nouhailaquahar@users.noreply.github.com>
2023-08-17 10:20:47 +0200

    Update test.component.ts

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class TestComponent {
+export class AppComponent {
+  constructor(private http: HttpClient) {}
 
+  triggerJenkinsPipeline() {
+    this.http.get('/api/trigger-pipeline').subscribe(() => {
+      alert('Pipeline Jenkins déclenchée avec succès !');
+    });
+  }
 }
